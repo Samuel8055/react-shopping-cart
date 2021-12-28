fb238cfec80fe8c79725b6920d8fec0ad9431d28
Samuel8055 <samueltschweitzer@gmail.com>
2021-12-28 16:22:20 +0530

    Apis creation

---
+++ backend/models/products.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+
+const productSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: shortid.generate,
+  },
+  title: String,
+  description: String,
+  image: String,
+  price: Number,
+  availableSizes: [String],
+});
+
+const Product = mongoose.model("products", productSchema);
+
+module.exports = Product;

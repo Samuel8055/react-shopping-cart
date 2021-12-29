a1c6f595ea06697226cd10747e34b7dee34edecb
Samuel8055 <samueltschweitzer@gmail.com>
2021-12-29 23:50:48 +0530

    Handled redux for filters, addtocart, removefromcart

--- src/redux/reducers/productReducers.js
+++ src/redux/reducers/productReducers.js
@@ -1,10 +1,27 @@
-import { FETCH_PRODUCTS } from "../types";
+import {
+  FETCH_PRODUCTS,
+  FILTER_PRODUCTS_BY_ORDER,
+  FILTER_PRODUCTS_BY_SIZE,
+} from "../types";
 
 export const productsReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_PRODUCTS:
       return {
         items: action.payload,
+        filteredItems: null,
+      };
+
+    case FILTER_PRODUCTS_BY_SIZE:
+      return {
+        ...state,
+        filteredItems: action.payload,
+      };
+
+    case FILTER_PRODUCTS_BY_ORDER:
+      return {
+        ...state,
+        filteredItems: action.payload,
       };
 
     default:
